498e490c06db6c6265a2fc246e041d2fdcc104b2
LanceShu <869924650@qq.com>
2017-09-13 22:37:59 +0800

    地球可以旋转

--- app/src/main/java/com/example/lance/earthforandroid/MainActivity.java
+++ app/src/main/java/com/example/lance/earthforandroid/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
-import com.unity3d.player.UnityPlayer;
 import com.unity3d.player.UnityPlayerActivity;
 
 public class MainActivity extends UnityPlayerActivity {
